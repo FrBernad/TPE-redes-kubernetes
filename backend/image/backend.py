f8f11428db1279cb057f8be9ce46c5ad96991129
frbernad <frbernad@itba.edu.ar>
2023-05-15 18:41:11 +0000

    fix: database fields naming

--- backend/image/backend.py
+++ backend/image/backend.py
@@ -7,9 +7,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@database/mov
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Movie(db.Model):
+class Movies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
     director = db.Column(db.String(255))
     year = db.Column(db.Integer)
     name = db.Column(db.String(200), nullable=False)
@@ -31,11 +30,13 @@ def get_movies():
     if not title:
         return jsonify({'error': 'Missing title parameter'}), 400
 
-    movies = Movie.query.filter(or_(*[Movie.title.ilike('%{}%'.format(token)) for token in title.split()])).all()
+    movies = Movies.query.filter(or_(*[Movies.name.ilike('%{}%'.format(token)) for token in title.split()])).all()
 
     movies = movies[:size]
 
-    return jsonify(movies), 200
+    movies_json = [{'name': movie.name} for movie in movies]
+
+    return jsonify(movies_json), 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
