c70c8feb3b808d744e943b55d0a19ca1c32b083e
Ignacio Agustín Manfredi <imanfredi@itba.edu.ar>
2023-05-18 21:30:58 +0000

    Add songs api and namespace to nginx controller

---
+++ backend/movies/image/movies.py
@@ -0,0 +1,63 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import or_
+from marshmallow import Schema, fields
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@database/db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Movies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    director = db.Column(db.String(255))
+    year = db.Column(db.Integer)
+    name = db.Column(db.String(200), nullable=False)
+    genre =   db.Column(db.String(60), nullable=False)
+    score  =  db.Column(db.NUMERIC)
+    director = db.Column(db.String(150), nullable=True)
+    writer   = db.Column(db.String(150), nullable=True)
+    star     = db.Column(db.String(150), nullable=True)
+    company  = db.Column(db.String(120), nullable=True)
+    country = db.Column(db.String(80), nullable=True)
+
+    def __repr__(self):
+        return '<Movie %r>' % self.title
+
+class MoviesSchema(Schema):
+    id = fields.Int(dump_only=True)
+    director = fields.Str()
+    year = fields.Int()
+    name = fields.Str(required=True)
+    genre = fields.Str(required=True)
+    score = fields.Float()
+    writer = fields.Str()
+    star = fields.Str()
+    company = fields.Str()
+    country = fields.Str()
+
+@app.route('/movies', methods=['GET'])
+def get_movies():
+    title = request.args.get('title')
+    size = request.args.get('size', default=10, type=int)
+    if not title:
+        return jsonify({'error': 'Missing title parameter'}), 400
+
+    movies = Movies.query.filter(or_(*[Movies.name.ilike('%{}%'.format(token)) for token in title.split()])).all()
+
+    movies = movies[:size]
+
+    schema = MoviesSchema(many=True)
+    serialized_movies = schema.dump(movies)
+
+    return serialized_movies, 200
+
+@app.route('/', methods=['GET'])
+def get_api_specification():
+    specs = {
+        "movies_url": "http://api.movies.com/movies?title={title}{&size}",
+    }
+    return specs, 200
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080, host='0.0.0.0')
