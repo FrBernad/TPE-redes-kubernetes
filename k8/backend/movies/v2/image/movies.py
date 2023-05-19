9f2a137fc08926ab57e279f161d32518a97a9730
frbernad <frbernad@itba.edu.ar>
2023-05-19 23:43:02 +0000

    feat: add secret for service database info and external db servicec

--- k8/backend/movies/v2/image/movies.py
+++ k8/backend/movies/v2/image/movies.py
@@ -1,10 +1,17 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
 from marshmallow import Schema, fields
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@database-service/db'
+
+database_user = os.environ["POSTGRES_USER"]
+database_password = os.environ["POSTGRES_PASS"]
+database_db = os.environ["POSTGRES_DB"]
+database_host = os.environ["POSTGRES_HOST"]
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{database_user}:{database_password}@{database_host}/{database_db}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -37,26 +44,45 @@ class MoviesSchema(Schema):
 
 @app.route('/movies', methods=['GET'])
 def get_movies():
-    title = request.args.get('title')
+    name = request.args.get('name')
     size = request.args.get('size', default=10, type=int)
-    if not title:
-        return jsonify({'error': 'Missing title parameter'}), 400
 
-    movies = Movies.query.filter(or_(*[Movies.name.ilike('%{}%'.format(token)) for token in title.split()])).all()
+    if not name:
+        response = jsonify({'error': 'Missing name parameter'})
+        response.status_code = 400
+        response.headers.extend(_get_response_headers())
+        return response
+
+    movies = Movies.query.filter(or_(*[Movies.name.ilike('%{}%'.format(token)) for token in name.split()])).all()
 
     movies = movies[:size]
 
     schema = MoviesSchema(many=True)
     serialized_movies = schema.dump(movies)
 
-    return serialized_movies, 200
+    response = make_response(serialized_movies, 200)
+    response.headers.extend(_get_response_headers())
+
+    return response
 
 @app.route('/', methods=['GET'])
 def get_api_specification():
     specs = {
-        "movies_url": "http://api.movies.com/v2/movies?title={title}{&size}",
+        "movies_url": "http://api.movies.com/v2/movies?name={name}{&size}",
     }
-    return specs, 200
+
+    response = make_response(specs, 200)
+
+    response.headers.extend(_get_response_headers())
+
+    return response
+
+def _get_response_headers():
+    return {
+        'POD-IP': os.environ.get('POD_IP'),
+        'POD-NAME': os.environ.get('POD_NAME'),
+        'NODE-NAME': os.environ.get('NODE_NAME'),
+    }
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='0.0.0.0')
